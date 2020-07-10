b8524855e3b8331d335d0ce050205c361aa3a164
Hafid24 <ilymassh@gmail.com>
2020-07-10 03:22:52 +0100

    Bootstraping, adding initial components and enable a linter

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <div v-for="q in data">{{q}}</div>
+  </div>
+</template>
+
+<script>
+import Indicator from './components/Indicator.vue'
+import Data from './arabicData.json'
+
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      data: []
+    }
+  },
+ methods:{
+    init(){
+      this.data.push('jhg');
+      this.data = Data;
+      console.log(this.data);
+    }
+  },
+  mounted(){
+    this.init()
+  }
+}
+
+
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Indicator.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="indicatorContainer flex flex-column"/>
+    <div class="flex">
+      <div class="progress-bar"/>      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'indicator',
+  data () {
+    return {
+      position: 0,
+    }
+  },
+  methods:{
+    changePosition(){
+       
+    }
+  },
+}
+</script>
+
+<style>
+.progress-bar {
+  width: 100%;
+  height: 5px;
+  background-color: orange;
+  position: relative;
+}
+
+</style>
